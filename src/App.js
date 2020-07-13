20cc1f3e5554e76c740db37a80caaec77466ee67
James Song <nothorse@gmail.com>
2020-07-13 22:50:08 +0900

    componet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+    id: 1,
+    name: '홍길동',
+    image: 'https://placeimg.com/64/64/',
+    age: 19,
+    gender: '남',
+    job: '대학생'
+   },
+   {
+    id: 2,
+    name: '홍길서',
+    image: 'https://placeimg.com/64/64/',
+    age: 13,
+    gender: '남',
+    job: '대학생'
+   },
+   {
+    id: 3,
+    name: '홍길남',
+    image: 'https://placeimg.com/64/64/',
+    age: 15,
+    gender: '남',
+    job: '대학생'
+   },
+   {
+    id: 4,
+    name: '홍길북',
+    image: 'https://placeimg.com/64/64/',
+    age: 15,
+    gender: '남',
+    job: '대학생'
+   }
+  ];
 
 function App() {
   return (
-    <div className="gray-background"> 
-      <img src={logo} lat="logo" />
-      <h2>Let' go</h2>
+    <div> 
+    {
+      customers.map(data => {
+        return(
+        <Customer
+        key={data.id}
+        id={data.id}
+        customer={data}
+          />
+        );
+        
+      })
+    }
     </div>
   );
 }
